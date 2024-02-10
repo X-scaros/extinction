43f1f07725cbe0965328861a459a66fff335addc
X-scaros <75679626+X-scaros@users.noreply.github.com>
2024-02-10 12:56:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,28 @@ const path = require('path');
 
 console.log(__dirname);
 
-const logPath = path.join(__dirname, 'TheIsle', 'Saved', 'Logs', 'TheIsle.log');
+const logPath = path.join(__dirname, '..', 'X-tinction', 'TheIsle', 'Saved', 'Logs', 'TheIsle.log');
 const playersPath = path.join(__dirname, 'players');
 
 const getPlayables = () => {
   const filePath = path.join(__dirname, 'playables.txt');
+
   const text = fs.readFileSync(filePath, 'utf8');
+
   const playables = text.split(', ');
 
   return playables;
 };
 
 const monitorLog = () => {
-  fs.watch(logPath, (eventType, filename) => {
-    console.log(eventType);
-    console.log(filename);
+  const watcher = fs.watch(logPath);
+
+  watcher.on('change', () => {
+    console.log('change');
   });
 };
 
 const playables = getPlayables();
+console.log(playables);
 
 monitorLog();
